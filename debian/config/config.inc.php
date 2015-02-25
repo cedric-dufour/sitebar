2f9ea6db0212b97a7d681e59fe6ae9609a790a99
Cédric Dufour <cedric.dufour@ced-network.net>
2015-02-25 11:33:35 +0100

    Debianize upstream (https://github.com/brablc/sitebar)

---
+++ debian/config/config.inc.php
@@ -0,0 +1,14 @@
+<?php
+$SITEBAR = array
+(
+    'db' => array
+    (
+        'host'      =>  'localhost',
+        'name'      =>  'sitebar',
+        'username'  =>  'sitebar',
+        'password'  =>  '',
+    ),
+    'baseurl' => null,
+    'login_as' => null,
+);
+
